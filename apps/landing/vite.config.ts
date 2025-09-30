a428448dfcaf370a027369f64779177cc83c4ce2
Festus Gitahi <festus@gitahi.cc>
2025-09-30 23:55:07 +0300

    feat(landing): integrate Tailwind CSS and update app structure; remove unused assets and styles

--- apps/landing/vite.config.ts
+++ apps/landing/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      "@/": fileURLToPath(new URL('./src', import.meta.url)),
+      "@/assets": fileURLToPath(new URL('./src/assets', import.meta.url)),
+    },
+  },
+  plugins: [react(), svgr()],
 })
