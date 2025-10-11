9c28247cb8695bb42c4270929542cf846bbf1ef3
Festus Gitahi <festus@gitahi.cc>
2025-10-11 23:22:33 +0300

    feat(shared): implement asset management system with S3 and local storage support; add configuration, logging, and type validation for assets

---
+++ packages/shared/langs.ts
@@ -0,0 +1,8 @@
+export const langNameMappings: Record<string, string> = {
+    en: "English",
+    en_US: "English (US)",
+    fr: "French",
+  };
+  
+  export const supportedLangs = Object.keys(langNameMappings);
+  
